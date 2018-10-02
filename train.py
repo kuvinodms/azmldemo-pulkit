0e30e4997375b947aa22323ee7b5d59401fd102f
Pulkit Agarwal <Pulkit.Agarwal@microsoft.com>
2018-10-02 19:07:14 +0000

    Updated train.py

--- train.py
+++ train.py
@@ -1,51 +1,41 @@
-'''
-Code sample is based from gender classification challenge for 'Learn Python for Data Science #1' 
-by @Sirajology on https://youtu.be/T5pRlIbr6gg
-'''
-
-from sklearn.svm import SVC
-import pickle
-
-# height, width, shoe size
-X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39],
-     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
-
-Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female', 'female', 'male', 'female']
-
-
-
-clf = SVC()
-clf = clf.fit(X, Y)
-
-print('Predicted value:', clf.predict([[190, 70, 43]]))
-print('Accuracy', clf.score(X,Y))
-
-print('Export the model to model.pkl')
-f = open('model.pkl', 'wb')
-pickle.dump(clf, f)
-f.close()
-
-print('Import the model from model.pkl')
-f2 = open('model.pkl', 'rb')
-clf2 = pickle.load(f2)
-
-X_new = [[154, 54, 35]]
-print('New Sample:', X_new)
-print('Predicted class:', clf2.predict(X_new))
-
+from sklearn.datasets import load_diabetes
+from sklearn.linear_model import Ridge
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.externals import joblib
 
 # Import Run from azureml.core, 
 # and get handle of current run for logging and history purposes
 from azureml.core.run import Run
 run = Run.get_submitted_run()
 
+X, y = load_diabetes(return_X_y=True)
+
+columns = ['age', 'gender', 'bmi', 'bp', 's1', 's2', 's3', 's4', 's5', 's6']
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+data = {"train":{"x":x_train, "y":y_train},
+        "test":{"x":x_test, "y":y_test}}
+
+alpha = 0.55
+
+reg = Ridge(alpha=alpha)
+reg.fit(data["train"]["x"], data["train"]["y"])
+print('Fitted a model')
+
+preds = reg.predict(data["test"]["x"])
+mse = mean_squared_error(preds, data["test"]["y"])
+
+# Log metrics
+run.log("alpha", alpha)
+run.log("mse", mse)
+run.log("columns", columns)
+
 model_path = "model.pkl"
 
 # Save model as part of the run history
 with open(model_path, "wb") as file:
-    from sklearn.externals import joblib
-    joblib.dump(clf, file)
-run.upload_file(model_path,  model_path)
-os.remove(model_path)
+    joblib.dump(value=reg, filename=os.path.join('./outputs/',
+                                                    model_path))
 
-
+print('Validated a model, mean squared error: ',mse)
